dc351a1faa29ca00dae882ee4ae98acab9d9af1c
Hemchand200 <hemchand2001304@gmail.com>
2020-04-11 11:30:10 -0700

    Search,Delete and Update Fetaures

--- Todo_List.cpp
+++ Todo_List.cpp
@@ -79,6 +79,106 @@ void readData() {
     read.close();
 }
 
+int searchData(bool printResult = true) {
+    printHeader("SEARCH TASK BY ID");
+
+    int id;
+    cout << "\n\tEnter task ID: ";
+    cin >> id;
+
+    todo todo;
+    ifstream read("todo.txt");
+
+    while (read >> todo.id) {
+        read.ignore();
+        getline(read, todo.task);
+        if (todo.id == id) {
+            if (printResult) {
+                print(todo);
+            }
+            read.close();
+            return id;
+        }
+    }
+
+    read.close();
+    cout << "\n\tTask not found.\n";
+    return -1;
+}
+
+void deleteData() {
+    printHeader("DELETE TASK");
+
+    int id = searchData();
+    if (id == -1) return;
+
+    cout << "\n\tDo you want to delete this task? (y/n): ";
+    char choice;
+    cin >> choice;
+
+    if (choice == 'y' || choice == 'Y') {
+        todo todo;
+        ofstream tempFile("temp.txt");
+        ifstream read("todo.txt");
+
+        while (read >> todo.id) {
+            read.ignore();
+            getline(read, todo.task);
+            if (todo.id != id) {
+                tempFile << todo.id << "\n" << todo.task << "\n";
+            }
+        }
+
+        read.close();
+        tempFile.close();
+        remove("todo.txt");
+        rename("temp.txt", "todo.txt");
+
+        cout << "\n\tTask deleted successfully.\n";
+    } else {
+        cout << "\n\tTask not deleted.\n";
+    }
+}
+
+void updateData() {
+    printHeader("UPDATE TASK");
+
+    int id = searchData();
+    if (id == -1) return;
+
+    cout << "\n\tDo you want to update this task? (y/n): ";
+    char choice;
+    cin >> choice;
+
+    if (choice == 'y' || choice == 'Y') {
+        cin.ignore();
+        string newTask;
+        cout << "\n\tEnter updated task description: ";
+        getline(cin, newTask);
+
+        todo todo;
+        ofstream temp("temp.txt");
+        ifstream read("todo.txt");
+
+        while (read >> todo.id) {
+            read.ignore();
+            getline(read, todo.task);
+            if (todo.id == id) {
+                todo.task = newTask;
+            }
+            temp << todo.id << "\n" << todo.task << "\n";
+        }
+
+        read.close();
+        temp.close();
+        remove("todo.txt");
+        rename("temp.txt", "todo.txt");
+
+        cout << "\n\tTask updated successfully.\n";
+    } else {
+        cout << "\n\tTask not updated.\n";
+    }
+}
 
 int main() {
     system("Color E0"); // Yellow background, black text
@@ -98,7 +198,10 @@ int main() {
         cout << "\n\n\tMENU";
         cout << "\n\t1. Add a Task";
         cout << "\n\t2. View All Tasks";
-        cout << "\n\t3. Exit";
+        cout << "\n\t3. Search a Task by ID";
+        cout << "\n\t4. Delete a Task";
+        cout << "\n\t5. Update a Task";
+        cout << "\n\t6. Exit";
         cout << "\n\n\tEnter your choice: ";
 
         int choice;
@@ -107,7 +210,10 @@ int main() {
         switch (choice) {
             case 1: addTodo(); break;
             case 2: readData(); break;
-            case 3: cout << "\n\tExiting... Goodbye!\n"; return 0;
+            case 3: searchData(); break;
+            case 4: deleteData(); break;
+            case 5: updateData(); break;
+            case 6: cout << "\n\tExiting... Goodbye!\n"; return 0;
             default: cout << "\n\tInvalid choice. Please enter between 1 to 6.\n";
         }
 
